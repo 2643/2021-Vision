6c8dce5f79526f62e82b686980748f03c7b81ec0
probablyanasian <43358667+probablyanasian@users.noreply.github.com>
2021-04-02 21:24:11 -0700

    I can type

--- duplicate_stream.sh
+++ duplicate_stream.sh
@@ -5,7 +5,7 @@ fi
 
 rmmod v4l2loopback
 modprobe v4l2loopback video_nr=21,22,23,24,25
-ffmpeg -f video4linux2 -i /dev/v4l2/by-id/usb-046d_HD_Pro_Webcam_C920_5AB6901F-video-index0 \
+ffmpeg -f video4linux2 -i /dev/v4l/by-id/usb-046d_HD_Pro_Webcam_C920_5AB6901F-video-index0 \
 -codec copy -f v4l2 /dev/video21 \
 -codec copy -f v4l2 /dev/video22 \
 -codec copy -f v4l2 /dev/video23 \
