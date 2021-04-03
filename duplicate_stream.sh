282dd06a6822347fe1659bfbc20488c9a2681b4f
probablyanasian <43358667+probablyanasian@users.noreply.github.com>
2021-04-02 23:43:07 -0700

    fix.

--- duplicate_stream.sh
+++ duplicate_stream.sh
@@ -6,10 +6,11 @@ if [ "$EUID" -ne 0 ]
 fi
 
 rmmod v4l2loopback
-modprobe v4l2loopback video_nr=21,22,23,24,25
+modprobe v4l2loopback video_nr=20,21,22,23,24,30
+# /dev/video30 is used for 2020-vision, not in this repo nor 2020-vision.
 ffmpeg -f video4linux2 -i /dev/v4l/by-id/usb-046d_HD_Pro_Webcam_C920_5AB6901F-video-index0 \
+-codec copy -f v4l2 /dev/video20 \
 -codec copy -f v4l2 /dev/video21 \
 -codec copy -f v4l2 /dev/video22 \
 -codec copy -f v4l2 /dev/video23 \
--codec copy -f v4l2 /dev/video24 \
 -codec copy -f v4l2 /dev/video25
