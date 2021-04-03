7d7aecc379b5412a5b0715b7bde00180316eb4ba
probablyanasian <43358667+probablyanasian@users.noreply.github.com>
2021-04-02 22:19:56 -0700

    test?

--- duplicate_stream.sh
+++ duplicate_stream.sh
@@ -1,8 +1,6 @@
 #/bin/bash
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit 1
-fi
+echo "$EUID"
+echo "if not 0, close and restart with root/sudo"
 
 rmmod v4l2loopback
 modprobe v4l2loopback video_nr=21,22,23,24,25
