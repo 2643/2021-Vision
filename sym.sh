8c1c48c48be4ac1283206296dbf768c3dfde93de
probablyanasian <43358667+probablyanasian@users.noreply.github.com>
2021-04-02 22:55:57 -0700

    fix

--- sym.sh
+++ sym.sh
@@ -6,7 +6,7 @@ if [ "$EUID" -ne 0 ]
 fi
 
 chmod a+x duplicate_stream.sh
-ln ./split-camera.service /etc/systemd/system/
-ln ./galactic_search/gs_ball_tracking.service /etc/systemd/system/
-ln ./galactic_search/gs_marker_tracking.service /etc/systemd/system/
-ln ./bounce_path/bounce_path_code_portion.service /etc/systemd/system/
+ln -sf ./split-camera.service /etc/systemd/system/
+ln -sf ./galactic_search/gs_ball_tracking.service /etc/systemd/system/
+ln -sf ./galactic_search/gs_marker_tracking.service /etc/systemd/system/
+ln -sf ./bounce_path/bounce_path_code_portion.service /etc/systemd/system/
