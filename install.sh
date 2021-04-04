1fe749294d9b8f4a28c4eaeda7391a7653736ebe
probablyanasian <43358667+probablyanasian@users.noreply.github.com>
2021-04-03 21:38:42 -0700

    am dumb.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ "$EUID" -ne 0 ]
   exit 1
 fi
 
-apt-get install vim ffmpeg python3 python3-pip libatlas-base-dev
+apt-get install vim ffmpeg python3 python3-pip libatlas-base-dev v4l2loopback-dkms
 pip3 install pynetworktables imutils scikit-build
 pip3 install opencv-python
 
