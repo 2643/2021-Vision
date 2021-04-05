9aabe3939a9976ef62ae6cc7f6b1786a70242c1e
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2021-04-05 15:01:08 -0700

    camera id

--- duplicate_stream.sh
+++ duplicate_stream.sh
@@ -6,12 +6,12 @@ if [ "$EUID" -ne 0 ]
 fi
 
 rmmod v4l2loopback
-v4l2-ctl -d /dev/v4l/by-id/usb-046d_HD_Pro_Webcam_C920_5AB6901F-video-index0 --set-ctrl=exposure_auto=1  --set-ctrl=exposure_absolute=640
+v4l2-ctl -d /dev/v4l/by-id/usb-046d_HD_Pro_Webcam_C920_4EDBE93F-video-index0 --set-ctrl=exposure_auto=1  --set-ctrl=exposure_absolute=640
 # I have no idea how to use exposure_absolute.
 modprobe v4l2loopback video_nr=20,21,22,23,24,30
 # /dev/video30 is used for 2020-vision, not in this repo nor 2020-vision.
 # ffmpeg -f video4linux2 -i /dev/v4l/by-id/usb-046d_HD_Pro_Webcam_C920_4EDBE93F-video-index0 \
-ffmpeg -f video4linux2 -i /dev/v4l/by-id/usb-046d_HD_Pro_Webcam_C920_5AB6901F-video-index0 \
+ffmpeg -f video4linux2 -i /dev/v4l/by-id/usb-046d_HD_Pro_Webcam_C920_4EDBE93F-video-index0 \
 -codec copy -f v4l2 /dev/video20 \
 -codec copy -f v4l2 /dev/video21 \
 -codec copy -f v4l2 /dev/video22 \
